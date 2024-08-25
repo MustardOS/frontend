f83d4b633da9574fc8f3bcb4b6eb7e8ad038dbce
tr12345 <27497732+GrumpyGopher@users.noreply.github.com>
2024-08-24 23:23:43 -0400

    updated muxtester to use ui_common

--- muxtester/theme.c
+++ muxtester/theme.c
@@ -15,113 +15,13 @@ struct small {
 };
 
 void apply_theme() {
-    struct big background_elements[] = {
-            {ui_scrTester, theme.SYSTEM.BACKGROUND},
-            {ui_pnlFooter, theme.FOOTER.BACKGROUND},
-            {ui_pnlHeader, theme.HEADER.BACKGROUND},
-    };
-    for (size_t i = 0; i < sizeof(background_elements) / sizeof(background_elements[0]); ++i) {
-        lv_obj_set_style_bg_color(background_elements[i].e, lv_color_hex(background_elements[i].c),
-                                  LV_PART_MAIN | LV_STATE_DEFAULT);
-    }
-
-    struct small background_alpha_elements[] = {
-            {ui_scrTester, theme.SYSTEM.BACKGROUND_ALPHA},
-            {ui_pnlFooter, theme.FOOTER.BACKGROUND_ALPHA},
-            {ui_pnlHeader, theme.HEADER.BACKGROUND_ALPHA},
-    };
-    for (size_t i = 0; i < sizeof(background_alpha_elements) / sizeof(background_alpha_elements[0]); ++i) {
-        lv_obj_set_style_bg_opa(background_alpha_elements[i].e, background_alpha_elements[i].c,
-                                LV_PART_MAIN | LV_STATE_DEFAULT);
-    }
-
-    struct big default_elements[] = {
-            {ui_lblButton,    theme.LIST_DEFAULT.TEXT},
-            {ui_lblFirst,     theme.LIST_DEFAULT.TEXT},
-            {ui_lblDatetime,  theme.DATETIME.TEXT},
-            {ui_lblMessage,   theme.MESSAGE.TEXT},
-            {ui_lblTitle,     theme.HEADER.TEXT},
-            {ui_staBluetooth, theme.STATUS.BLUETOOTH.NORMAL},
-            {ui_staNetwork,   theme.STATUS.NETWORK.NORMAL},
-            {ui_staCapacity,  theme.STATUS.BATTERY.NORMAL},
-    };
-    for (size_t i = 0; i < sizeof(default_elements) / sizeof(default_elements[0]); ++i) {
-        lv_obj_set_style_text_color(default_elements[i].e, lv_color_hex(default_elements[i].c),
-                                    LV_PART_MAIN | LV_STATE_DEFAULT);
-    }
-
     struct small text_default_alpha_elements[] = {
             {ui_lblButton,    theme.LIST_DEFAULT.TEXT_ALPHA},
             {ui_lblFirst,     theme.LIST_DEFAULT.TEXT_ALPHA},
-            {ui_lblTitle,     theme.HEADER.TEXT_ALPHA},
-            {ui_lblMessage,   theme.MESSAGE.TEXT_ALPHA},
-            {ui_lblDatetime,  theme.DATETIME.ALPHA},
-            {ui_staBluetooth, theme.STATUS.BLUETOOTH.NORMAL_ALPHA},
-            {ui_staNetwork,   theme.STATUS.NETWORK.NORMAL_ALPHA},
-            {ui_staCapacity,  theme.STATUS.BATTERY.NORMAL_ALPHA},
     };
     for (size_t i = 0; i < sizeof(text_default_alpha_elements) / sizeof(text_default_alpha_elements[0]); ++i) {
         lv_obj_set_style_text_opa(text_default_alpha_elements[i].e, text_default_alpha_elements[i].c,
                                   LV_PART_MAIN | LV_STATE_DEFAULT);
-    }
-
-    struct small font_pad_top_header_elements[] = {
-            {ui_lblTitle,    theme.FONT.HEADER_PAD_TOP},
-            {ui_lblDatetime, theme.FONT.HEADER_PAD_TOP},
-    };
-    for (size_t i = 0; i < sizeof(font_pad_top_header_elements) / sizeof(font_pad_top_header_elements[0]); ++i) {
-        lv_obj_set_style_pad_top(font_pad_top_header_elements[i].e, font_pad_top_header_elements[i].c,
-                                 LV_PART_MAIN | LV_STATE_DEFAULT);
-    }
-
-    struct small font_pad_bottom_header_elements[] = {
-            {ui_lblTitle,    theme.FONT.HEADER_PAD_BOTTOM},
-            {ui_lblDatetime, theme.FONT.HEADER_PAD_BOTTOM},
-    };
-    for (size_t i = 0;
-         i < sizeof(font_pad_bottom_header_elements) / sizeof(font_pad_bottom_header_elements[0]); ++i) {
-        lv_obj_set_style_pad_bottom(font_pad_bottom_header_elements[i].e, font_pad_bottom_header_elements[i].c,
-                                    LV_PART_MAIN | LV_STATE_DEFAULT);
-    }
-
-    struct small font_pad_top_header_icon_elements[] = {
-            {ui_staBluetooth, theme.FONT.HEADER_ICON_PAD_TOP},
-            {ui_staNetwork,   theme.FONT.HEADER_ICON_PAD_TOP},
-            {ui_staCapacity,  theme.FONT.HEADER_ICON_PAD_TOP},
-    };
-    for (size_t i = 0;
-         i < sizeof(font_pad_top_header_icon_elements) / sizeof(font_pad_top_header_icon_elements[0]); ++i) {
-        lv_obj_set_style_pad_top(font_pad_top_header_icon_elements[i].e, font_pad_top_header_icon_elements[i].c,
-                                 LV_PART_MAIN | LV_STATE_DEFAULT);
-    }
-
-    struct small font_pad_bottom_header_icon_elements[] = {
-            {ui_staBluetooth, theme.FONT.HEADER_ICON_PAD_BOTTOM},
-            {ui_staNetwork,   theme.FONT.HEADER_ICON_PAD_BOTTOM},
-            {ui_staCapacity,  theme.FONT.HEADER_ICON_PAD_BOTTOM},
-    };
-    for (size_t i = 0;
-         i < sizeof(font_pad_bottom_header_icon_elements) / sizeof(font_pad_bottom_header_icon_elements[0]); ++i) {
-        lv_obj_set_style_pad_bottom(font_pad_bottom_header_icon_elements[i].e,
-                                    font_pad_bottom_header_icon_elements[i].c,
-                                    LV_PART_MAIN | LV_STATE_DEFAULT);
-    }
-
-    struct small font_pad_top_message_elements[] = {
-            {ui_lblMessage, theme.FONT.MESSAGE_PAD_TOP},
-    };
-    for (size_t i = 0; i < sizeof(font_pad_top_message_elements) / sizeof(font_pad_top_message_elements[0]); ++i) {
-        lv_obj_set_style_pad_top(font_pad_top_message_elements[i].e, font_pad_top_message_elements[i].c,
-                                 LV_PART_MAIN | LV_STATE_DEFAULT);
-    }
-
-    struct small font_pad_bottom_message_elements[] = {
-            {ui_lblMessage, theme.FONT.MESSAGE_PAD_BOTTOM},
-    };
-    for (size_t i = 0;
-         i < sizeof(font_pad_bottom_message_elements) / sizeof(font_pad_bottom_message_elements[0]); ++i) {
-        lv_obj_set_style_pad_bottom(font_pad_bottom_message_elements[i].e, font_pad_bottom_message_elements[i].c,
-                                    LV_PART_MAIN | LV_STATE_DEFAULT);
     }
 
     struct small font_pad_top_list_elements[] = {
@@ -138,65 +38,5 @@ void apply_theme() {
     for (size_t i = 0; i < sizeof(font_pad_bottom_list_elements) / sizeof(font_pad_bottom_list_elements[0]); ++i) {
         lv_obj_set_style_pad_bottom(font_pad_bottom_list_elements[i].e, font_pad_bottom_list_elements[i].c,
                                     LV_PART_MAIN | LV_STATE_DEFAULT);
-    }
-
-    struct small content_pad_left_element[] = {
-            {ui_pnlContent, theme.MISC.CONTENT.PADDING_LEFT},
-    };
-    for (size_t i = 0; i < sizeof(content_pad_left_element) / sizeof(content_pad_left_element[0]); ++i) {
-        lv_obj_set_style_pad_left(content_pad_left_element[i].e, content_pad_left_element[i].c,
-                                  LV_PART_MAIN | LV_STATE_DEFAULT);
-    }
-
-    struct small content_pad_right_element[] = {
-            {ui_pnlContent, theme.MISC.CONTENT.WIDTH},
-    };
-    for (size_t i = 0; i < sizeof(content_pad_right_element) / sizeof(content_pad_right_element[0]); ++i) {
-        lv_obj_set_style_width(content_pad_right_element[i].e, content_pad_right_element[i].c,
-                               LV_PART_MAIN | LV_STATE_DEFAULT);
-    }
-
-    struct small datetime_pad_left_element[] = {
-            {ui_lblDatetime, theme.DATETIME.PADDING_LEFT},
-    };
-    for (size_t i = 0; i < sizeof(datetime_pad_left_element) / sizeof(datetime_pad_left_element[0]); ++i) {
-        lv_obj_set_style_pad_left(datetime_pad_left_element[i].e, datetime_pad_left_element[i].c,
-                                  LV_PART_MAIN | LV_STATE_DEFAULT);
-    }
-
-    struct small status_pad_right_element[] = {
-            {ui_conGlyphs, theme.STATUS.PADDING_RIGHT},
-    };
-    for (size_t i = 0; i < sizeof(status_pad_right_element) / sizeof(status_pad_right_element[0]); ++i) {
-        lv_obj_set_style_pad_right(status_pad_right_element[i].e, status_pad_right_element[i].c,
-                                   LV_PART_MAIN | LV_STATE_DEFAULT);
-    }
-
-    struct small navigation_alignment_element[] = {
-            {ui_pnlFooter, theme.NAV.ALIGNMENT},
-    };
-    for (size_t i = 0; i < sizeof(navigation_alignment_element) / sizeof(navigation_alignment_element[0]); ++i) {
-        lv_flex_align_t e_align;
-        switch (navigation_alignment_element[i].c) {
-            case 1:
-                e_align = LV_FLEX_ALIGN_CENTER;
-                break;
-            case 2:
-                e_align = LV_FLEX_ALIGN_END;
-                break;
-            case 3:
-                e_align = LV_FLEX_ALIGN_SPACE_AROUND;
-                break;
-            case 4:
-                e_align = LV_FLEX_ALIGN_SPACE_BETWEEN;
-                break;
-            case 5:
-                e_align = LV_FLEX_ALIGN_SPACE_EVENLY;
-                break;
-            default:
-                e_align = LV_FLEX_ALIGN_START;
-                break;
-        }
-        lv_obj_set_style_flex_main_place(navigation_alignment_element[i].e, e_align, LV_PART_MAIN | LV_STATE_DEFAULT);
     }
 }
--- muxtester/ui/ui.h
+++ muxtester/ui/ui.h
@@ -16,41 +16,13 @@ extern "C" {
 #include "ui_events.h"
 
 // SCREEN: ui_scrTester
-void ui_scrTester_screen_init(void);
-extern lv_obj_t * ui_scrTester;
-extern lv_obj_t * ui_pnlWall;
-extern lv_obj_t * ui_imgWall;
-extern lv_obj_t * ui_pnlHeader;
-extern lv_obj_t * ui_lblDatetime;
-extern lv_obj_t * ui_lblTitle;
-extern lv_obj_t * ui_conGlyphs;
-extern lv_obj_t * ui_staBluetooth;
-extern lv_obj_t * ui_staNetwork;
-extern lv_obj_t * ui_staCapacity;
-extern lv_obj_t * ui_pnlContent;
+void ui_scrTester_screen_init(lv_obj_t * ui_pnlContent);
 extern lv_obj_t * ui_lblButton;
 extern lv_obj_t * ui_lblFirst;
-extern lv_obj_t * ui_pnlFooter;
-extern lv_obj_t * ui_lblMessage;
-extern lv_obj_t * ui____initial_actions0;
 
-LV_IMG_DECLARE(ui_img_nothing_png);    // assets/nothing.png
-LV_IMG_DECLARE(ui_img_muoskofi_png);    // assets/muoskofi.png
-
-
-LV_FONT_DECLARE(ui_font_Awesome);
-LV_FONT_DECLARE(ui_font_AwesomeBig);
-LV_FONT_DECLARE(ui_font_AwesomeBrand);
-LV_FONT_DECLARE(ui_font_AwesomeBrandSmall);
-LV_FONT_DECLARE(ui_font_AwesomeSmall);
 LV_FONT_DECLARE(ui_font_Gamepad);
-LV_FONT_DECLARE(ui_font_GamepadNav);
-LV_FONT_DECLARE(ui_font_NotoSans);
-LV_FONT_DECLARE(ui_font_NotoSansBig);
-LV_FONT_DECLARE(ui_font_NotoSansSmall);
 
-
-void ui_init(void);
+void ui_init(lv_obj_t * ui_pnlContent);
 
 #ifdef __cplusplus
 } /*extern "C"*/
