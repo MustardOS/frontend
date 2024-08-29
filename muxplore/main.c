5459b93f9ec4a1db90b2ff33611f2bc0c703e2ba
GrumpyGopher <27497732+GrumpyGopher@users.noreply.github.com>
2024-08-29 01:34:04 -0400

    muOS Frontend
    * Fixed issue with translations not displaying for Favourites and History
    
    * Fixed issue with muxapp not loading application if language other than English selected

--- muxplore/main.c
+++ muxplore/main.c
@@ -1998,7 +1998,7 @@ int main(int argc, char *argv[]) {
 
     load_config(&config);
     load_theme(&theme, &config, &device, mux_prog);
-    load_language(mux_prog);
+    load_language("muxplore");
 
     ui_common_screen_init(&theme, &device, "");
     ui_init(ui_screen, &theme);
