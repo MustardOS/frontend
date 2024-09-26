MustardOS/frontend

commit f3fd2d928a297c0e46f66e840ecf8e8eb57d6ee8

Fix signed/unsigned comparison bug with device.INPUT.AXIS (#66)
* Change AXIS max value to signed to fix comparisons

* Add missing breaks in input event switches

muxarchive/main.c
@@ -347,12 +347,9 @@ void joystick_task() {
                                 }
                             }
                         }
+                        break;
                     case EV_ABS:
                         if (msgbox_active) {
-                            break;
-                        }
-                        if ((ev.code == NAV_DPAD_VER || ev.code == NAV_ANLG_VER) &&
-                            (ev.value > -device.INPUT.AXIS && ev.value < device.INPUT.AXIS)) {
                             break;
                         }
                         if (ev.code == ABS_Y) {
@@ -362,9 +359,7 @@ void joystick_task() {
                             break;
                         }
                         if (ev.code == NAV_DPAD_VER || ev.code == NAV_ANLG_VER) {
-                            if ((ev.value >= -device.INPUT.AXIS &&
-                                 ev.value <= -device.INPUT.AXIS) ||
-                                ev.value == -1) {
+                            if (ev.value == -device.INPUT.AXIS || ev.value == -1) {
                                 if (current_item_index == 0) {
                                     current_item_index = ui_count - 1;
                                     nav_prev(ui_group, 1);
@@ -378,9 +373,7 @@ void joystick_task() {
                                     JOYUP_pressed = (ev.value != 0);
                                     list_nav_prev(1);
                                 }
-                            } else if ((ev.value >= (device.INPUT.AXIS) &&
-                                        ev.value <= (device.INPUT.AXIS)) ||
-                                       ev.value == 1) {
+                            if (ev.value == device.INPUT.AXIS || ev.value == 1) {
                                 if (current_item_index == ui_count - 1) {
                                     current_item_index = 0;
                                     nav_next(ui_group, 1);
@@ -399,6 +392,7 @@ void joystick_task() {
                                 JOYDOWN_pressed = 0;
                             }
                         }
+                        break;
                     default:
                         break;
                 }

muxapp/main.c
@@ -348,12 +348,9 @@ void joystick_task() {
                                 }
                             }
                         }
+                        break;
                     case EV_ABS:
                         if (msgbox_active) {
-                            break;
-                        }
-                        if ((ev.code == NAV_DPAD_VER || ev.code == NAV_ANLG_VER) &&
-                            (ev.value > -device.INPUT.AXIS && ev.value < device.INPUT.AXIS)) {
                             break;
                         }
                         if (ev.code == ABS_Y) {
@@ -363,9 +360,7 @@ void joystick_task() {
                             break;
                         }
                         if (ev.code == NAV_DPAD_VER || ev.code == NAV_ANLG_VER) {
-                            if ((ev.value >= -device.INPUT.AXIS &&
-                                 ev.value <= -device.INPUT.AXIS) ||
-                                ev.value == -1) {
+                            if (ev.value == -device.INPUT.AXIS || ev.value == -1) {
                                 if (current_item_index == 0) {
                                     current_item_index = ui_count - 1;
                                     nav_prev(ui_group, 1);
@@ -378,9 +373,7 @@ void joystick_task() {
                                     JOYUP_pressed = (ev.value != 0);
                                     list_nav_prev(1);
                                 }
-                            } else if ((ev.value >= (device.INPUT.AXIS) &&
-                                        ev.value <= (device.INPUT.AXIS)) ||
-                                       ev.value == 1) {
+                            } else if (ev.value == device.INPUT.AXIS || ev.value == 1) {
                                 if (current_item_index == ui_count - 1) {
                                     current_item_index = 0;
                                     nav_next(ui_group, 1);
@@ -398,6 +391,7 @@ void joystick_task() {
                                 JOYDOWN_pressed = 0;
                             }
                         }
+                        break;
                     default:
                         break;
                 }

muxassign/main.c
@@ -647,12 +647,9 @@ void joystick_task() {
                                 }
                             }
                         }
+                        break;
                     case EV_ABS:
                         if (msgbox_active) {
-                            break;
-                        }
-                        if ((ev.code == NAV_DPAD_VER || ev.code == NAV_ANLG_VER) &&
-                            (ev.value > -device.INPUT.AXIS && ev.value < device.INPUT.AXIS)) {
                             break;
                         }
                         if (ev.code == ABS_Y) {
@@ -662,9 +659,7 @@ void joystick_task() {
                             break;
                         }
                         if (ev.code == NAV_DPAD_VER || ev.code == NAV_ANLG_VER) {
-                            if ((ev.value >= -device.INPUT.AXIS &&
-                                 ev.value <= -device.INPUT.AXIS) ||
-                                ev.value == -1) {
+                            if (ev.value == -device.INPUT.AXIS || ev.value == -1) {
                                 if (current_item_index == 0) {
                                     current_item_index = ui_count - 1;
                                     nav_prev(ui_group, 1);
@@ -676,9 +671,7 @@ void joystick_task() {
                                     JOYUP_pressed = (ev.value != 0);
                                     list_nav_prev(1);
                                 }
-                            } else if ((ev.value >= (device.INPUT.AXIS) &&
-                                        ev.value <= (device.INPUT.AXIS)) ||
-                                       ev.value == 1) {
+                            } else if (ev.value == device.INPUT.AXIS || ev.value == 1) {
                                 if (current_item_index == ui_count - 1) {
                                     current_item_index = 0;
                                     nav_next(ui_group, 1);
@@ -695,6 +688,7 @@ void joystick_task() {
                                 JOYDOWN_pressed = 0;
                             }
                         }
+                        break;
                     default:
                         break;
                 }

muxconfig/main.c
@@ -285,12 +285,9 @@ void joystick_task() {
                                 }
                             }
                         }
+                        break;
                     case EV_ABS:
                         if (msgbox_active) {
-                            break;
-                        }
-                        if ((ev.code == NAV_DPAD_VER || ev.code == NAV_ANLG_VER) &&
-                            (ev.value > -device.INPUT.AXIS && ev.value < device.INPUT.AXIS)) {
                             break;
                         }
                         if (ev.code == ABS_Y) {
@@ -300,9 +297,7 @@ void joystick_task() {
                             break;
                         }
                         if (ev.code == NAV_DPAD_VER || ev.code == NAV_ANLG_VER) {
-                            if ((ev.value >= -device.INPUT.AXIS &&
-                                 ev.value <= -device.INPUT.AXIS) ||
-                                ev.value == -1) {
+                            if (ev.value == -device.INPUT.AXIS || ev.value == -1) {
                                 if (current_item_index == 0) {
                                     current_item_index = ui_count - 1;
                                     nav_prev(ui_group, 1);
@@ -316,9 +311,7 @@ void joystick_task() {
                                     list_nav_prev(1);
                                     nav_moved = 1;
                                 }
-                            } else if ((ev.value >= (device.INPUT.AXIS) &&
-                                        ev.value <= (device.INPUT.AXIS)) ||
-                                       ev.value == 1) {
+                            } else if (ev.value == device.INPUT.AXIS || ev.value == 1) {
                                 if (current_item_index == ui_count - 1) {
                                     current_item_index = 0;
                                     nav_next(ui_group, 1);
@@ -336,6 +329,7 @@ void joystick_task() {
                                 JOYUP_pressed = 0;
                                 JOYDOWN_pressed = 0;
                             }
+                            break;
                         }
                     default:
                         break;

muxgov/main.c
@@ -484,12 +484,9 @@ void joystick_task() {
                                 }
                             }
                         }
+                        break;
                     case EV_ABS:
                         if (msgbox_active) {
-                            break;
-                        }
-                        if ((ev.code == NAV_DPAD_VER || ev.code == NAV_ANLG_VER) &&
-                            (ev.value > -device.INPUT.AXIS && ev.value < device.INPUT.AXIS)) {
                             break;
                         }
                         if (ev.code == ABS_Y) {
@@ -499,9 +496,7 @@ void joystick_task() {
                             break;
                         }
                         if (ev.code == NAV_DPAD_VER || ev.code == NAV_ANLG_VER) {
-                            if ((ev.value >= -device.INPUT.AXIS &&
-                                 ev.value <= -device.INPUT.AXIS) ||
-                                ev.value == -1) {
+                            if (ev.value == -device.INPUT.AXIS || ev.value == -1) {
                                 if (current_item_index == 0) {
                                     current_item_index = ui_count - 1;
                                     nav_prev(ui_group, 1);
@@ -513,9 +508,7 @@ void joystick_task() {
                                     JOYUP_pressed = (ev.value != 0);
                                     list_nav_prev(1);
                                 }
-                            } else if ((ev.value >= (device.INPUT.AXIS) &&
-                                        ev.value <= (device.INPUT.AXIS)) ||
-                                       ev.value == 1) {
+                            } else if (ev.value == device.INPUT.AXIS || ev.value == 1) {
                                 if (current_item_index == ui_count - 1) {
                                     current_item_index = 0;
                                     nav_next(ui_group, 1);
@@ -532,6 +525,7 @@ void joystick_task() {
                                 JOYDOWN_pressed = 0;
                             }
                         }
+                        break;
                     default:
                         break;
                 }

muxinfo/main.c
@@ -260,12 +260,9 @@ void joystick_task() {
                                 }
                             }
                         }
+                        break;
                     case EV_ABS:
                         if (msgbox_active) {
-                            break;
-                        }
-                        if ((ev.code == NAV_DPAD_VER || ev.code == NAV_ANLG_VER) &&
-                            (ev.value > -device.INPUT.AXIS && ev.value < device.INPUT.AXIS)) {
                             break;
                         }
                         if (ev.code == ABS_Y) {
@@ -275,9 +272,7 @@ void joystick_task() {
                             break;
                         }
                         if (ev.code == NAV_DPAD_VER || ev.code == NAV_ANLG_VER) {
-                            if ((ev.value >= -device.INPUT.AXIS &&
-                                 ev.value <= -device.INPUT.AXIS) ||
-                                ev.value == -1) {
+                            if (ev.value == -device.INPUT.AXIS || ev.value == -1) {
                                 if (current_item_index == 0) {
                                     current_item_index = UI_COUNT - 1;
                                     nav_prev(ui_group, 1);
@@ -291,9 +286,7 @@ void joystick_task() {
                                     list_nav_prev(1);
                                     nav_moved = 1;
                                 }
-                            } else if ((ev.value >= (device.INPUT.AXIS) &&
-                                        ev.value <= (device.INPUT.AXIS)) ||
-                                       ev.value == 1) {
+                            } else if (ev.value == device.INPUT.AXIS || ev.value == 1) {
                                 if (current_item_index == UI_COUNT - 1) {
                                     current_item_index = 0;
                                     nav_next(ui_group, 1);
@@ -312,6 +305,7 @@ void joystick_task() {
                                 JOYDOWN_pressed = 0;
                             }
                         }
+                        break;
                     default:
                         break;
                 }

muxlanguage/main.c
@@ -265,12 +265,9 @@ void joystick_task() {
                                 }
                             }
                         }
+                        break;
                     case EV_ABS:
                         if (msgbox_active) {
-                            break;
-                        }
-                        if ((ev.code == NAV_DPAD_VER || ev.code == NAV_ANLG_VER) &&
-                            (ev.value > -device.INPUT.AXIS && ev.value < device.INPUT.AXIS)) {
                             break;
                         }
                         if (ev.code == ABS_Y) {
@@ -280,9 +277,7 @@ void joystick_task() {
                             break;
                         }
                         if (ev.code == NAV_DPAD_VER || ev.code == NAV_ANLG_VER) {
-                            if ((ev.value >= -device.INPUT.AXIS &&
-                                 ev.value <= -device.INPUT.AXIS) ||
-                                ev.value == -1) {
+                            if (ev.value == -device.INPUT.AXIS || ev.value == -1) {
                                 if (current_item_index == 0) {
                                     current_item_index = ui_count - 1;
                                     nav_prev(ui_group, 1);
@@ -294,9 +289,7 @@ void joystick_task() {
                                     JOYUP_pressed = (ev.value != 0);
                                     list_nav_prev(1);
                                 }
-                            } else if ((ev.value >= (device.INPUT.AXIS) &&
-                                        ev.value <= (device.INPUT.AXIS)) ||
-                                       ev.value == 1) {
+                            } else if (ev.value == device.INPUT.AXIS || ev.value == 1) {
                                 if (current_item_index == ui_count - 1) {
                                     current_item_index = 0;
                                     nav_next(ui_group, 1);
@@ -313,6 +306,7 @@ void joystick_task() {
                                 JOYDOWN_pressed = 0;
                             }
                         }
+                        break;
                     default:
                         break;
                 }

muxlaunch/main.c
@@ -306,18 +306,13 @@ void joystick_task() {
                                 }
                             }
                         }
+                        break;
                     case EV_ABS:
                         if (msgbox_active) {
                             break;
                         }
-                        if ((ev.code == NAV_DPAD_VER || ev.code == NAV_ANLG_VER) &&
-                            (ev.value > -device.INPUT.AXIS && ev.value < device.INPUT.AXIS)) {
-                            break;
-                        }
                         if (ev.code == NAV_DPAD_VER || ev.code == NAV_ANLG_VER) {
-                            if ((ev.value >= -device.INPUT.AXIS &&
-                                 ev.value <= -device.INPUT.AXIS) ||
-                                ev.value == -1) {
+                            if (ev.value == -device.INPUT.AXIS || ev.value == -1) {
                                 // Horizontal Navigation with 2 rows of 4 items.  Wrap on Row.
                                 if (theme.MISC.NAVIGATION_TYPE == 4 &&
                                     (current_item_index == 0 || current_item_index == 4)) {
@@ -331,10 +326,7 @@ void joystick_task() {
                                 } else {
                                     list_nav_prev(1);
                                 }
-
-                            } else if ((ev.value >= device.INPUT.AXIS &&
-                                        ev.value <= device.INPUT.AXIS) ||
-                                       ev.value == 1) {
+                            } else if (ev.value == device.INPUT.AXIS || ev.value == 1) {
                                 // Horizontal Navigation with 2 rows of 4 items.  Wrap on Row.
                                 if (theme.MISC.NAVIGATION_TYPE == 4 &&
                                     (current_item_index == 3 || current_item_index == 7)) {
@@ -353,22 +345,16 @@ void joystick_task() {
                         // Horizontal Navigation with 2 rows of 4 items
                         if ((theme.MISC.NAVIGATION_TYPE == 2 || theme.MISC.NAVIGATION_TYPE == 4) &&
                             (ev.code == NAV_DPAD_HOR || ev.code == NAV_ANLG_HOR)) {
-                            if ((ev.value >= -device.INPUT.AXIS &&
-                                 ev.value <= -device.INPUT.AXIS) ||
-                                ev.value == -1) {
+                            if (ev.value == -device.INPUT.AXIS || ev.value == -1) {
                                 list_nav_prev(4);
-                            } else if ((ev.value >= (device.INPUT.AXIS) &&
-                                        ev.value <= (device.INPUT.AXIS)) ||
-                                       ev.value == 1) {
+                            } else if (ev.value == device.INPUT.AXIS || ev.value == 1) {
                                 list_nav_next(4);
                             }
                         }
                         // Horizontal Navigation with 3 item first row, 5 item second row
                         if ((theme.MISC.NAVIGATION_TYPE == 3 || theme.MISC.NAVIGATION_TYPE == 5) &&
                             (ev.code == NAV_DPAD_HOR || ev.code == NAV_ANLG_HOR)) {
-                            if ((ev.value >= -device.INPUT.AXIS &&
-                                 ev.value <= -device.INPUT.AXIS) ||
-                                ev.value == -1) {
+                            if (ev.value == -device.INPUT.AXIS || ev.value == -1) {
                                 switch (current_item_index) {
                                     case 3:
                                     case 4:
@@ -382,9 +368,7 @@ void joystick_task() {
                                         list_nav_prev(5);
                                         break;
                                 }
-                            } else if ((ev.value >= (device.INPUT.AXIS) &&
-                                        ev.value <= (device.INPUT.AXIS)) ||
-                                       ev.value == 1) {
+                            } else if (ev.value == device.INPUT.AXIS || ev.value == 1) {
                                 switch (current_item_index) {
                                     case 0:
                                         list_nav_next(3);
@@ -398,6 +382,7 @@ void joystick_task() {
                                 }
                             }
                         }
+                        break;
                     default:
                         break;
                 }

commit 19af1c574e1b45fe2f8ff19111dedb6ac85dda32

Fix broken muxarchive build
My fault, I broke this in #66. :(

muxarchive/main.c
@@ -373,7 +373,7 @@ void joystick_task() {
                                     JOYUP_pressed = (ev.value != 0);
                                     list_nav_prev(1);
                                 }
-                            if (ev.value == device.INPUT.AXIS || ev.value == 1) {
+                            } else if (ev.value == device.INPUT.AXIS || ev.value == 1) {
                                 if (current_item_index == ui_count - 1) {
                                     current_item_index = 0;
                                     nav_next(ui_group, 1);