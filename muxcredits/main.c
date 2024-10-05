bf09affa3f39e7db493cce6760cb1d92a1d97c33
Jonathan Rascher <jon@bcat.name>
2024-10-05 17:37:04 -0500

    Add input to muxcredits for quit hotkey

--- muxcredits/main.c
+++ muxcredits/main.c
@@ -1,11 +1,13 @@
 #include "../lvgl/lvgl.h"
 #include "../lvgl/drivers/display/fbdev.h"
 #include "ui/ui.h"
+#include <fcntl.h>
 #include <string.h>
 #include <stdlib.h>
 #include "../common/common.h"
 #include "../common/config.h"
 #include "../common/device.h"
+#include "../common/input.h"
 
 int NAV_DPAD_HOR;
 int NAV_ANLG_HOR;
@@ -19,7 +21,6 @@ int msgbox_active = 0;
 int input_disable = 0;
 int SD2_found = 0;
 int nav_sound = 0;
-int safe_quit = 0;
 int bar_header = 0;
 int bar_footer = 0;
 char *osd_message;
@@ -29,9 +30,16 @@ struct mux_device device;
 
 lv_obj_t *msgbox_element = NULL;
 
+void timeout_task(lv_timer_t *timer) {
+    mux_input_stop();
+}
+
+void handle_quit(void) {
+    mux_input_stop();
+}
+
 int main() {
     load_device(&device);
-
 
     lv_init();
     fbdev_init(device.SCREEN.DEVICE);
@@ -64,15 +72,36 @@ int main() {
 
     if (!config.BOOT.FACTORY_RESET) write_text_to_file(MUOS_PDI_LOAD, "w", CHAR, "credit");
 
-    int hold = 0;
-    while (!safe_quit) {
-        hold++;
-        refresh_screen();
-        if (hold > 310000) {
-            safe_quit = 1;
-        }
-        //printf("%d\n", hold);
+    lv_timer_create(timeout_task, 70000, NULL);
+
+    int js_fd = open(device.INPUT.EV1, O_RDONLY);
+    if (js_fd < 0) {
+        perror("Failed to open joystick device");
+        return 1;
     }
+
+    int js_fd_sys = open(device.INPUT.EV0, O_RDONLY);
+    if (js_fd_sys < 0) {
+        perror("Failed to open joystick device");
+        return 1;
+    }
+
+    mux_input_options input_opts = {
+            .gamepad_fd = js_fd,
+            .system_fd = js_fd_sys,
+            .max_idle_ms = 16 /* ~60 FPS */,
+            .combo = {
+                    {
+                            .type_mask = BIT(MUX_INPUT_MENU_LONG) | BIT(MUX_INPUT_START),
+                            .press_handler = handle_quit,
+                    },
+            },
+            .idle_handler = refresh_screen,
+    };
+    mux_input_task(&input_opts);
+
+    close(js_fd);
+    close(js_fd_sys);
 
     return 0;
 }
