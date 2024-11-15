01f75d0d8e8c635efe21c83b78e401afedd3a93b
GrumpyGopher <27497732+GrumpyGopher@users.noreply.github.com>
2024-11-15 17:12:26 -0500

    consolidate grid mode input navigation handler (#149)

--- common/input/list_nav.h
+++ common/input/list_nav.h
@@ -1,5 +1,7 @@
 #pragma once
 
+extern int grid_mode_enabled;
+
 void handle_list_nav_up(void);
 
 void handle_list_nav_down(void);
@@ -7,6 +9,14 @@ void handle_list_nav_down(void);
 void handle_list_nav_up_hold(void);
 
 void handle_list_nav_down_hold(void);
+
+void handle_list_nav_left();
+
+void handle_list_nav_right();
+
+void handle_list_nav_left_hold(void);
+
+void handle_list_nav_right_hold(void);
 
 void handle_list_nav_page_up(void);
 
