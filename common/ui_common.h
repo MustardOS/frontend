b8a40e9ba319e7e27eada3fe64f9f1fcdddd23f7
GrumpyGopher <27497732+GrumpyGopher@users.noreply.github.com>
2025-01-03 20:59:01 -0500

    update theme engine to fall back to 640x480 (#170)

--- common/ui_common.h
+++ common/ui_common.h
@@ -40,6 +40,7 @@ void create_grid_panel(struct theme_config *theme, int item_count);
 void create_grid_item(struct theme_config *theme, lv_obj_t *cell_pnl, lv_obj_t *cell_label, lv_obj_t *cell_image,
                       int16_t col, int16_t row, char *item_image_path, char *item_image_focused_path, char *item_text);
 
+extern lv_obj_t *ui_screen_container;
 extern lv_obj_t *ui_screen;
 extern lv_obj_t *ui_pnlWall;
 extern lv_obj_t *ui_imgWall;
