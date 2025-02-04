dcb142b7e25d7042b89d09e8dac37dbdbd018344
tr12345 <27497732+GrumpyGopher@users.noreply.github.com>
2025-02-03 19:15:16 -0500

    update muxsplash to center image

--- module/muxsplash.c
+++ module/muxsplash.c
@@ -58,8 +58,7 @@ int main(int argc, char *argv[]) {
     char init_wall[MAX_BUFFER_SIZE];
     snprintf(init_wall, sizeof(init_wall), "M:%s", argv[1]);
 
-    lv_obj_t *img = lv_img_create(ui_screen);
-    lv_img_set_src(img, init_wall);
+    lv_img_set_src(ui_imgWall, init_wall);
 
 #if TEST_IMAGE
     display_testing_message(ui_screen);
