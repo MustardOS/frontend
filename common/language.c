4f5409a32a28dfe24ba822e3941daad63a503fa8
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2025-02-15 21:08:57 +1000

    Added missing language fields

--- common/language.c
+++ common/language.c
@@ -229,10 +229,12 @@ void load_lang(struct mux_lang *lang) {
     SPECIFIC_FIELD(lang->MUXINFO.TITLE, "INFORMATION");
     SPECIFIC_FIELD(lang->MUXINFO.SYSTEM, "System Details");
     SPECIFIC_FIELD(lang->MUXINFO.ACTIVITY, "Activity Tracker");
+    SPECIFIC_FIELD(lang->MUXINFO.SCREENSHOT, "Screenshots");
     SPECIFIC_FIELD(lang->MUXINFO.INPUT, "Input Tester");
     SPECIFIC_FIELD(lang->MUXINFO.CREDIT, "Supporters");
     SPECIFIC_FIELD(lang->MUXINFO.HELP.SYSTEM, "Access version information and system details");
     SPECIFIC_FIELD(lang->MUXINFO.HELP.ACTIVITY, "Access statistics of played content and other activity");
+    SPECIFIC_FIELD(lang->MUXINFO.HELP.SCREENSHOT, "View all of the screenshots taken on the device");
     SPECIFIC_FIELD(lang->MUXINFO.HELP.INPUT, "Test the controls of the device");
     SPECIFIC_FIELD(lang->MUXINFO.HELP.CREDIT, "View all of the current muOS supporters");
 
