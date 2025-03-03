9340abf66201b6e77c06a6c446ad689f754d9990
tr12345 <27497732+GrumpyGopher@users.noreply.github.com>
2025-03-03 09:55:03 -0500

    fix invalid reference in muxtask

--- module/muxtask.c
+++ module/muxtask.c
@@ -161,7 +161,7 @@ void create_task_items() {
             lv_obj_t *ui_lblTaskItemGlyph = lv_img_create(ui_pnlTask);
             if (ui_lblTaskItemGlyph) {
                 apply_theme_list_glyph(&theme, ui_lblTaskItemGlyph, mux_module,
-                                       get_var_from_file(task_path, items[i].extra_data, "ICON", "task"));
+                                       get_script_value(items[i].extra_data, "ICON", "task"));
             }
 
             lv_group_add_obj(ui_group, ui_lblTaskItem);
