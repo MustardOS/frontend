7f9492f55866b76dd70fb2483a721f5a498d0e40
GrumpyGopher <27497732+GrumpyGopher@users.noreply.github.com>
2025-05-31 03:00:11 -0400

    update options to use info items to launch configs (#260)

--- module/ui/ui_muxoption.c
+++ module/ui/ui_muxoption.c
@@ -15,6 +15,10 @@ lv_obj_t *ui_icoCore_option;
 lv_obj_t *ui_icoGovernor_option;
 lv_obj_t *ui_icoTag_option;
 
+lv_obj_t *ui_lblCore_option_value;
+lv_obj_t *ui_lblGovernor_option_value;
+lv_obj_t *ui_lblTag_option_value;
+
 void init_muxoption(lv_obj_t *ui_pnlContent) {
     ui_pnlSearch_option = lv_obj_create(ui_pnlContent);
     ui_pnlCore_option = lv_obj_create(ui_pnlContent);
@@ -34,4 +38,11 @@ void init_muxoption(lv_obj_t *ui_pnlContent) {
     ui_icoCore_option = lv_img_create(ui_pnlCore_option);
     ui_icoGovernor_option = lv_img_create(ui_pnlGovernor_option);
     ui_icoTag_option = lv_img_create(ui_pnlTag_option);
+
+    ui_lblCore_option_value = lv_label_create(ui_pnlCore_option);
+    lv_label_set_text(ui_lblCore_option_value, "");
+    ui_lblGovernor_option_value = lv_label_create(ui_pnlGovernor_option);
+    lv_label_set_text(ui_lblGovernor_option_value, "");
+    ui_lblTag_option_value = lv_label_create(ui_pnlTag_option);
+    lv_label_set_text(ui_lblTag_option_value, "");
 }
