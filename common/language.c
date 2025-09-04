ec3921651f8dd829539f8a152acad31c04ae682f
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2025-09-04 13:11:15 +1000

    Fixed language ellipsis

--- common/language.c
+++ common/language.c
@@ -613,7 +613,7 @@ void load_lang(struct mux_lang *lang) {
     SPECIFIC_FIELD(lang->MUXPICKER.INVALID_RES, "Incompatible Theme Resolution Detected");
     SPECIFIC_FIELD(lang->MUXPICKER.HOLD_CONFIRM, "Hold L2 and press X to confirm deletion!");
     SPECIFIC_FIELD(lang->MUXPICKER.PROTECTED, "This theme is protected from deletion!");
-    SPECIFIC_FIELD(lang->MUXPICKER.FAILED, "Failed to remove item...");
+    SPECIFIC_FIELD(lang->MUXPICKER.FAILED, "Failed to remove item…");
     SPECIFIC_FIELD(lang->MUXPICKER.NONE.CREDIT, "There are no attributed credits!");
     SPECIFIC_FIELD(lang->MUXPICKER.NONE.CUSTOM, "No Custom Packages Found");
     SPECIFIC_FIELD(lang->MUXPICKER.NONE.CATALOGUE, "No Catalogue Packages Found");
