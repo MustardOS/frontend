d6826ac397ffddefa61f54200428f6ecf5224439
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2025-11-19 19:40:41 +1000

    Ignore language refresh on boards without network capability

--- module/muxlanguage.c
+++ module/muxlanguage.c
@@ -147,7 +147,7 @@ static void handle_b(void) {
 }
 
 static void handle_x(void) {
-    if (download_in_progress || msgbox_active || hold_call) return;
+    if (download_in_progress || msgbox_active || hold_call || !device.BOARD.HAS_NETWORK) return;
 
     if (is_network_connected()) {
         play_sound(SND_CONFIRM);
