75d73ebe731267cdafbdd13574ad99c29bf26785
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2025-11-22 17:33:49 +1000

    Updated contributor list

--- module/ui/ui_muxcredits.c
+++ module/ui/ui_muxcredits.c
@@ -281,8 +281,8 @@ const char *knight_two[] = {
 };
 
 const char *contributors[] = {
-        "bitter_bizarro", "corey", "duncanyoyo1", "gleepglop1000", "imcokeman", "joyrider3774",
-        "sundownersport", "xonglebongle",
+        "antikk", "bitter_bizarro", "corey", "duncanyoyo1", "gleepglop1000", "imcokeman",
+        "joyrider3774", "snowram", "sundownersport", "xonglebongle",
         NULL
 };
 
