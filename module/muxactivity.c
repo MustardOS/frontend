282e88ed60dcc9c9cdf75093363e89f8a9bbd5c8
xonglebongle <116428087+xonglebongle@users.noreply.github.com>
2025-12-14 16:56:45 +1000

    Fixed top launch integer value

--- module/muxactivity.c
+++ module/muxactivity.c
@@ -1078,8 +1078,8 @@ static void export_activity_html(void) {
     fprintf(f, "<tr><td>Top Content by Time</td><td>%s (%s)</td></tr>",
             gs.top_time, format_total_time(gs.top_time_value));
 
-    fprintf(f, "<tr><td>Top Content by Launch</td><td>%s (%s)</td></tr>",
-            gs.top_launch, gs.top_launch);
+    fprintf(f, "<tr><td>Top Content by Launch</td><td>%s (%d)</td></tr>",
+            gs.top_launch, gs.top_launch_value);
 
     fprintf(f, "<tr><td>Most Frequent Core</td><td>%s</td></tr>",
             global_core_value);
